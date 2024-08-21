fcf756e2689679fb23eca550ec70fe125f6f5c75
Emanuel Mesa <mesaemanuel922@gmail.com>
2024-08-20 21:02:20 -0500

    Ejercicio 14

--- script.js
+++ script.js
@@ -182,3 +182,19 @@ function ejercicio_13() {
     }
 
 }
+
+function ejercicio_14() {
+    let hora = prompt(" ingrese una hora (solo el número de la hora, de 0 a 23): ")
+    if (hora >= 6 && hora < 12) {
+        console.log("Buenos días")
+    }
+    else if (hora >= 12 && hora < 18) {
+        console.log("Buenas tardes")
+    }
+    else if ((hora >= 18 && hora < 24) || (hora >= 0 && hora < 6)) {
+        console.log("Buenas noches");
+    } else {
+        console.log("Hora no válida.");
+    }
+
+}
