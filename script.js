22fdef270a4e656506faa5fc3a099e2b48e0a8f1
Emanuel Mesa <mesaemanuel922@gmail.com>
2024-08-20 21:00:19 -0500

    Ejercicio 12

--- script.js
+++ script.js
@@ -148,3 +148,25 @@ function ejercicio_11() {
         console.log("El precio final es: " + (precioBase - descuento))
     }
 }
+
+function ejercicio_12() {
+    let ano = prompt('Ingresa un año')
+    function esBisiesto(anio) {
+        if (anio % 4 === 0) {
+            if (anio % 100 === 0) {
+                return anio % 400 === 0;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+    if (esBisiesto(ano)) {
+        console.log('El año es bisiesto')
+    }
+    else {
+        console.log('El ' + ano + ' no es bisiesto')
+    }
+
+}
