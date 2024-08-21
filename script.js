9e41f856336684ef029028c29292d8df88087309
Emanuel Mesa <mesaemanuel922@gmail.com>
2024-08-20 20:47:30 -0500

    Ejercicio 7

--- script.js
+++ script.js
@@ -31,42 +31,58 @@ function ejercicio_3() {
 
 function ejercicio_4() {
     let totalCompra = prompt("Ingrese el total de una compra: ")
-if (totalCompra > 100) {
-    let descuento = totalCompra * 0.1
-    totalCompra = totalCompra - descuento
-    console.log("Tienes un descuento del 10% y el total es: " + totalCompra)
-}
-else {
-    console.log("No tienes descuento")
-}
+    if (totalCompra > 100) {
+        let descuento = totalCompra * 0.1
+        totalCompra = totalCompra - descuento
+        console.log("Tienes un descuento del 10% y el total es: " + totalCompra)
+    }
+    else {
+        console.log("No tienes descuento")
+    }
 }
 
 function ejercicio_5() {
     let numeroUno = prompt("Ingrese el primer número: ")
-let numeroDos = prompt("Ingrese el segundo número: ")
-if (numeroUno > numeroDos) {
-    console.log("El primer número es mayor que el segundo")
-}
-else if (numeroUno < numeroDos) {
-    console.log("El segundo número es mayor que el primero")
-}
-else {
-    console.log("Los números son iguales")
-}
+    let numeroDos = prompt("Ingrese el segundo número: ")
+    if (numeroUno > numeroDos) {
+        console.log("El primer número es mayor que el segundo")
+    }
+    else if (numeroUno < numeroDos) {
+        console.log("El segundo número es mayor que el primero")
+    }
+    else {
+        console.log("Los números son iguales")
+    }
 }
 
 function ejercicio_6() {
     let clasificacionEdad = prompt("Ingresa tu edad: ")
-if (clasificacionEdad >= 0 && clasificacionEdad <= 12) {
-    console.log("Eres un niño")
+    if (clasificacionEdad >= 0 && clasificacionEdad <= 12) {
+        console.log("Eres un niño")
+    }
+    else if (clasificacionEdad > 12 && clasificacionEdad < 17) {
+        console.log("Eres un adolescente.")
+    }
+    else if (clasificacionEdad > 18 && clasificacionEdad < 64) {
+        console.log("Eres un adulto.")
+    }
+    else if (clasificacionEdad >= 65) {
+        console.log("Eres un adulto mayor.")
+    }
 }
-else if (clasificacionEdad > 12 && clasificacionEdad < 17) {
-    console.log("Eres un adolescente.")
+
+function ejercicio_7() {
+    let nota = prompt("Ingrese una nota entre 0 y 100: ")
+if (nota >= 90 && nota <= 100) {
+    console.log("Excelente.")
 }
-else if (clasificacionEdad > 18 && clasificacionEdad < 64) {
-    console.log("Eres un adulto.")
+else if (nota >= 70 && nota <= 89) {
+    console.log("Bueno.")
 }
-else if (clasificacionEdad >= 65) {
-    console.log("Eres un adulto mayor.")
+else if (nota >= 50 && nota <= 69) {
+    console.log("Regular.")
+}
+else if (nota >= 0 && nota <= 49) {
+    console.log("Insuficiente.")
 }
 }
