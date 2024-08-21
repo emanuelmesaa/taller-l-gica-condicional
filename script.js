2603525fef1358e9272b4ac79607fbb588171890
Emanuel Mesa <mesaemanuel922@gmail.com>
2024-08-20 20:40:14 -0500

    Ejercicio 2

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function ejercicio_1() {
         console.log("Eres menor de edad")
     }
 }
+
+function ejercicio_2() {
+    let numeroUsuario = prompt("Ingrese un número: ")
+    if (numeroUsuario >= 0) {
+        console.log("El número es positivo")
+    }
+    else {
+        console.log("El número es negativo)")
+    }
+}
