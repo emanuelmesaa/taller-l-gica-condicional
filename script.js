1d2e91a9f1c83e461100ccd2c54a130c931b0db7
Emanuel Mesa <mesaemanuel922@gmail.com>
2024-08-20 21:03:07 -0500

    Ejercicio 15

--- script.js
+++ script.js
@@ -198,3 +198,24 @@ function ejercicio_14() {
     }
 
 }
+
+function ejercicio_15() {
+    let peso = parseFloat(prompt("Ingrese su peso en kilogramos:"));
+    let altura = parseFloat(prompt("Ingrese su altura en metros:"));
+
+    let imc = peso / (altura * altura);
+
+    if (imc < 18.5) {
+        console.log("Bajo peso");
+    } 
+    else if (imc >= 18.5 && imc < 24.9) {
+        console.log("Peso normal");
+    } 
+    else if (imc >= 25 && imc <+ 29.9) {
+        console.log("Sobrepeso");
+    } 
+    else {
+        console.log("Obesidad");
+    } 
+
+}
